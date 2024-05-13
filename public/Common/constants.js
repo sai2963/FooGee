b96f3fc54e597ce81de87de82c344ab025f21aee
sai2963 <venkatsai0629@gmail.com>
2024-05-13 20:59:28 +0530

    Different files are created for individual components

--- public/Common/constants.js
+++ public/Common/constants.js
@@ -17,17 +17,17 @@ export const shimmer_card_unit = 20;
 export const shimmer_menu_card_unit = 4;
 
 // Github - username
-export const Github_UserName = "ChetanNada";
-export const Github_Repository_Name = "Namaste-React";
+export const Github_UserName = "sai2963";
+export const Github_Repository_Name = "React-Food-app";
 
 // Github API for User
 export const Github_API_User = "https://api.github.com/users/";
 
 // Social Media Links
-export const Linkedin_Link = "https://www.linkedin.com/in/chetannada/";
-export const Twitter_Link = "https://twitter.com/ChetanNada";
-export const Github_Link = "https://github.com/chetannada";
-export const Email_Link = "mailto:call2chetannada@gmail.com";
+export const Linkedin_Link = "https://www.linkedin.com/in/duduka-venkat-sai-182a71267/";
+//export const Twitter_Link = "https://twitter.com/ChetanNada";
+export const Github_Link = "https://github.com/sai2963";
+export const Email_Link = "mailto:venkatsai0629@gmail.com";
 
 // Github Authorization Token
 export const options = {
