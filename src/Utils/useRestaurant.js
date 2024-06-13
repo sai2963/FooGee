0ae2d83948edb6d40a5d308eb4d81b86435745a4
sai2963 <venkatsai0629@gmail.com>
2024-06-14 00:12:59 +0530

    api added to constant.js

--- src/Utils/useRestaurant.js
+++ src/Utils/useRestaurant.js
@@ -1,5 +1,5 @@
 import { useEffect,useState } from "react";
-
+import { FoodFire_Menu_AP_URL } from "../../public/Common/constants";
 const useRestaurant=  (id) =>{
     const [resInfo,setResInfo]= useState(null);
 
@@ -11,7 +11,7 @@ const useRestaurant=  (id) =>{
     },[id])
 
     const fetchData = async () =>{
-        const data = await fetch(`https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&submitAction=ENTER&restaurantId=${id}`)
+        const data = await fetch(`${FoodFire_Menu_AP_URL}=${id}`)
         const json = await data.json();
         
         setResInfo(json.data || null);
